5f57d6f39528d5849b5091a75486411839d21729
RedPhoenixQ <steve.dark.03@gmail.com>
2024-06-02 00:38:39 +0200

    Add pocketbase instance to locals
    The server side cannot use a global instance as auth changes between requests

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,15 @@
+import { PUBLIC_PB_URL } from '$env/static/public';
 import { createContext } from '$lib/trpc/context';
 import { router } from '$lib/trpc/router';
 import type { Handle } from '@sveltejs/kit';
+import PocketBase from 'pocketbase';
 import { createTRPCHandle } from 'trpc-sveltekit';
 
 const trpcHandle: Handle = createTRPCHandle({ router, createContext });
 
 export async function handle({ event, resolve }) {
-	return await trpcHandle({ event, resolve });
+	event.locals.pb = new PocketBase(PUBLIC_PB_URL);
+
+	const response = await trpcHandle({ event, resolve });
+	return response;
 }
