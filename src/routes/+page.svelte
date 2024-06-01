a63784ee61b4ff7eed45a68bb029b543331123d6
RedPhoenixQ <steve.dark.03@gmail.com>
2024-06-02 00:44:26 +0200

    Add test login to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script lang="ts">
+	import LoginForm from '$lib/components/auth/LoginForm.svelte';
+</script>
+
+<header>
+	<LoginForm />
+</header>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
