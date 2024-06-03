b143cf4083a66d5cd554ead8591ce45abafee449
RedPhoenixQ <steve.dark.03@gmail.com>
2024-06-03 14:43:50 +0200

    Add a user store to make model changes reactive

--- src/lib/pb.ts
+++ src/lib/pb.ts
@@ -1,5 +1,12 @@
 import { PUBLIC_PB_URL } from '$env/static/public';
 import { TypedPocketBase } from 'typed-pocketbase';
-import type { Schema } from './schema';
+import type { Schema, UsersResponse } from './schema';
+import { writable } from 'svelte/store';
 
 export const pb = new TypedPocketBase<Schema>(PUBLIC_PB_URL);
+
+export const user = writable(pb.authStore.model as UsersResponse | null);
+
+// TODO: Make sure the authmodel is from the users collection
+//       Important for multiple auth collenctions in the future or admin auth
+pb.authStore.onChange((_token, model) => user.set(model as UsersResponse | null));
