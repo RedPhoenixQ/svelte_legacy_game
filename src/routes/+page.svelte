f235fa3fce05196d4a2becce5ff405536f15dbc4
RedPhoenixQ <steve.dark.03@gmail.com>
2024-06-03 14:45:43 +0200

    Use new user store

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,13 @@
 	import LoginForm from '$lib/components/auth/LoginForm.svelte';
 	import RegisterForm from '$lib/components/auth/RegisterForm.svelte';
 	import LogoutButton from '$lib/components/auth/LogoutButton.svelte';
-	import { pb } from '$lib/pb';
+	import { user } from '$lib/pb';
 </script>
 
 <header>
 	<div>
-		{#if pb.authStore.model}
-			<!-- content here -->
-			Logged in as: {pb.authStore.model.username}
+		{#if $user}
+			Logged in as: {$user.username}
 		{:else}
 			Not logged in
 		{/if}
