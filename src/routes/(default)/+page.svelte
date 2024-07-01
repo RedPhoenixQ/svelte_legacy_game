217e128ca0aa007d8e8c0a6b67576141c8c5092c
RedPhoenixQ <steve.dark.03@gmail.com>
2024-07-01 15:00:21 +0200

    Remove header from page

--- src/routes/(default)/+page.svelte
+++ src/routes/(default)/+page.svelte
@@ -1,24 +1,2 @@
-<script lang="ts">
-	import LoginForm from '$lib/components/auth/LoginForm.svelte';
-	import RegisterForm from '$lib/components/auth/RegisterForm.svelte';
-	import LogoutButton from '$lib/components/auth/LogoutButton.svelte';
-	import { user } from '$lib/pb';
-</script>
-
-<header>
-	<div>
-		{#if $user}
-			Logged in as: {$user.username}
-		{:else}
-			Not logged in
-		{/if}
-	</div>
-	<LoginForm />
-	<RegisterForm
-		on:registered={(event) => alert('Registered!\n\n' + JSON.stringify(event.detail, undefined, 2))}
-	/>
-	<LogoutButton />
-</header>
-
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
